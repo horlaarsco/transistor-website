b41fe712f25edb4b1209bd27df439c786e0c69a5
Darey <horla@Dareys-MacBook-Pro.local>
2020-03-02 08:07:06 +0100

    feat:Homepage Script

---
+++ script.js
@@ -0,0 +1,8 @@
+function showham() {
+  var nav = document.getElementById("mobile-nav2");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
